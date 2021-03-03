275839e0e1f4e5e233635f5475232173b19cfd5c
niiwade <niilantewade77@gmail.com>
2021-03-03 16:21:02 +0000

    written mailabe obejctv

--- smtp-email-server/routes/routes.js
+++ smtp-email-server/routes/routes.js
@@ -1,16 +1,16 @@
 //Baseline requirements
 
-require('dotenv').config()
+require('dotenv').config();
 
 //routes.js 
-const router = require('express').Router()
-const path = require('path')
-const nodemailer = require('nodemailer')
+const router = require('express').Router();
+const path = require('path');
+const nodemailer = require('nodemailer');
 
 
 //authenication using your gmail credentials
 
-const transfer = {
+const transport = {
     //authenicates email sending
 
     host: 'smtp.gmail.com',
@@ -24,22 +24,46 @@ const transfer = {
         pass: process.env.SMTP_TO_PASSWORD,
     },
 
-}
+};
 
-const transferer = nodemailer.createTransfer(transfer)
-transferer.verify(
+//calling the transport function 
+
+const transporter = nodemailer.createTransport(transport);
+transporter.verify(
     (error, success) => {
         if (error) {
             //if error happens code stops
-            console.error(error)
+            console.error(error);
         } else {
             //this means code run
-            console.log('Ready to send mail!')
+            console.log('Ready to send mail!');
         }
     }
 )
 
 
 router.get('/', (req, res, next) => {
-    res.status(200).json({ msg: 'Working' })
+    res.status(200).json({ msg: 'Working' });
+});
+
+
+router.post('/', (req, res, next) => {
+    //make mailable object
+    const mail = {
+        from: process.env.STMP_FROM_EMAIL,
+        to: process.env.STMP_TO_EMAIL,
+        subject: "",
+        text: `
+            from: 
+            ${req.body.name}
+
+            contact details
+            email: ${req.body.email}
+            phone: ${req.body.tel}
+
+            message: ${req.body.message}`,
+    };
+
+    //send mail to recepient 
+    transporter.sendMail(mail)
 })
